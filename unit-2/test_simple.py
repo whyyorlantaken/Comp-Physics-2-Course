922e108e3fa486ba031a154914a6d8a9ea3a54f6
whyyorlantaken <whyyorlantaken@gmail.com>
2024-12-26 19:10:25 -0500

    Reviewed

--- unit-2/test_simple.py
+++ unit-2/test_simple.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python3
-
-# Test module
-# Funtion 1: defines an operation
-
 def multiply(a, b):
-   c = a * b
-   return c
-
-# Function 2: test function
+    return a*b
 
 def test_multiply():
-   """
-   Function to test our multiply operation.
-   """
-   assert multiply(2, 5) == 10
+    assert multiply(4, 6) == 24
 
-
+def test_multiply2():
+    assert multiply(5, 6) == 30
